550d70e8043af7588f99d67fc893fb7061908a01
achensetyawati <achenzetya@gmail.com>
2018-04-27 14:23:52 +0700

    add helper lib

---
+++ com.danliris.support.lib/Helpers/BasicService.cs
@@ -0,0 +1,94 @@
+﻿using Com.Moonlay.Models;
+using Com.Moonlay.NetCore.Lib.Service;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
+using com.danliris.support.lib.Helpers;
+
+namespace com.danliris.support.lib.Helpers
+{
+    public abstract class BasicService<TDbContext, TModel> : StandardEntityService<TDbContext, TModel>
+        where TDbContext : DbContext
+        where TModel : StandardEntity, IValidatableObject
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+
+        public BasicService(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        public abstract Tuple<List<TModel>, int, Dictionary<string, string>, List<string>> ReadModel(int Page = 1, int Size = 25, string Order = "{}", List<string> Select = null, string Keyword = null, string Filter = "{}");
+
+        public virtual async Task<int> CreateModel(TModel Model)
+        {
+            return await this.CreateAsync(Model);
+        }
+
+        public virtual async Task<TModel> ReadModelById(int Id)
+        {
+            return await this.GetAsync(Id);
+        }
+
+        public virtual async Task<int> UpdateModel(int Id, TModel Model)
+        {
+            return await this.UpdateAsync(Id, Model);
+        }
+
+        public virtual async Task<int> DeleteModel(int Id)
+        {
+            return await this.DeleteAsync(Id);
+        }
+
+        public virtual IQueryable<TModel> ConfigureSearch(IQueryable<TModel> Query, List<string> SearchAttributes, string Keyword)
+        {
+            /* Search with Keyword */
+            if (Keyword != null)
+            {
+                Query = Query.Where(General.BuildSearch(SearchAttributes), Keyword);
+            }
+            return Query;
+        }
+
+        public virtual IQueryable<TModel> ConfigureFilter(IQueryable<TModel> Query, Dictionary<string, string> FilterDictionary)
+        {
+            if (FilterDictionary != null && !FilterDictionary.Count.Equals(0))
+            {
+                foreach (var f in FilterDictionary)
+                {
+                    string Key = f.Key;
+                    object Value = f.Value;
+                    string filterQuery = string.Concat(string.Empty, Key, " == @0");
+
+                    Query = Query.Where(filterQuery, Value);
+                }
+            }
+            return Query;
+        }
+
+        public virtual IQueryable<TModel> ConfigureOrder(IQueryable<TModel> Query, Dictionary<string, string> OrderDictionary)
+        {
+            /* Default Order */
+            if (OrderDictionary.Count.Equals(0))
+            {
+                OrderDictionary.Add("_LastModifiedUtc", General.DESCENDING);
+
+                Query = Query.OrderByDescending(b => b._LastModifiedUtc);
+            }
+            /* Custom Order */
+            else
+            {
+                string Key = OrderDictionary.Keys.First();
+                string OrderType = OrderDictionary[Key];
+
+                Query = Query.OrderBy(string.Concat(Key, " ", OrderType));
+            }
+            return Query;
+        }
+    }
+}
