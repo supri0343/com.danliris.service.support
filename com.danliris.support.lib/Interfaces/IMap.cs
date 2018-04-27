7c7b0a2aa976942e9b783813652883a6cde7b162
achensetyawati <achenzetya@gmail.com>
2018-04-27 14:50:27 +0700

    interfaces lib

---
+++ com.danliris.support.lib/Interfaces/IMap.cs
@@ -0,0 +1,8 @@
+﻿namespace com.danliris.support.lib.Interfaces
+{
+    public interface IMap<TModel, TViewModel>
+    {
+        TViewModel MapToViewModel(TModel model);
+        TModel MapToModel(TViewModel viewModel);
+    }
+}
