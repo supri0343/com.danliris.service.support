f7fd83a5fd24dc8ec88ed889ed01b96b2c7d45e4
Andreasaja <under.block22@gmail.com>
2024-07-19 10:19:58 +0700

    add filter keyword BC23,261,262,40

--- com.danliris.support.lib/Services/Ceisa/TPB/BC40Service.cs
+++ com.danliris.support.lib/Services/Ceisa/TPB/BC40Service.cs
@@ -4,6 +4,7 @@ using com.danliris.support.lib.Models;
 using com.danliris.support.lib.Models.Ceisa.TPB;
 using com.danliris.support.lib.ViewModel.Ceisa;
 using com.danliris.support.lib.ViewModel.Ceisa.TPBViewModel;
+using Com.DanLiris.Service.Purchasing.Lib.Helpers;
 using Com.DanLiris.Service.support.lib.Services;
 using Com.Moonlay.Models;
 using Microsoft.EntityFrameworkCore;
@@ -48,7 +49,14 @@ namespace com.danliris.support.lib.Services.Ceisa.TPB
                 postedBy = string.IsNullOrWhiteSpace(m.postedBy) ? "-" : m.postedBy,
                 CreatedDate = m._CreatedUtc.ToString("dd-MMM-yyyy")
             }).OrderByDescending(x => x.nomorAju);
-                
+
+            //add filter search keyword
+            List<string> SearchAtt = new List<string>() { "namaPenerima", "nomorAju", "nomorDaftar" };
+
+            Query = QueryHelper<TPBViewModelList>.ConfigureSearch(Query, SearchAtt, Keyword);
+            //--------
+
+
             List<object> ListData = new List<object>();
             ListData.AddRange(Query.Select(s => new
             {
